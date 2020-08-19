f06c1659e030741bfec17e5c05822856a6bc38bc
jesusChristian18 <chris.jesus1997@gmail.com>
2020-08-19 16:31:36 -0500

    proyectoV3

--- app/screens/Account/Account.js
+++ app/screens/Account/Account.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import {View, Text} from "react-native";
+
 import * as firebase from "firebase";
 import UserGuest from "./UserGuest";
 import UserLogged from "./UserLogged";
--- app/screens/Account/UserGuest.js
+++ app/screens/Account/UserGuest.js
@@ -1,10 +1,63 @@
 import React from "react";
-import {View, Text} from "react-native";
+import {StyleSheet, View, ScrollView, Text, Image} from "react-native";
+import {Button} from "react-native-elements";
 
 export default function UserGuest(){
     return(
-        <View>
-            <Text>UserGuest...</Text>
-        </View>
+        <ScrollView centerContent={true} style={styles.viewBody}>
+            <Image 
+                source={require("../../../assets/img/user-guest.jpg")}
+                resizeMode="contain"
+                style={styles.image}
+            />
+            <Text style={styles.title}>Consulta tu perfil de 5 tenedores</Text>
+            <Text style={styles.descripcion}>
+                Bienvenidos a Pandarius, encuentra tu mejor restaurante y comentanos como te fue como cliente vip
+            </Text>
+            <View style={styles.viewBtn}>
+           <Button title="Ver tu perfil"
+           buttonStyle={styles.btnStyle}
+           containerStyle={styles.btnContainer}
+           onPress={() => console.log("Click!!")}
+           ></Button>
+            </View>
+        </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    viewBody: {
+        marginLeft: 30,
+        marginRight: 30,
+    },
+    image: {
+        height: 300,
+        width: "100%",
+        marginBottom: 40,
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 19,
+        marginBottom: 10,
+        textAlign: "center",
+    },
+    descripcion:{
+        textAlign: "center",
+        marginBottom: 20,
+        color:"#808080",
+       
+    },
+
+    btnStyle: {
+        backgroundColor: "#00a680"
+    },
+    btnContainer: {
+        width: "70%",
+    },
+
+    viewBtn: {
+        flex: 1,
+        alignItems: "center",
+    }
+});
+
